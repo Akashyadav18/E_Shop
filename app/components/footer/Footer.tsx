8b53309009da7e2667c69edba54bb0f135acb4bb
Akashyadav18 <shadowlee5469@gmail.com>
2023-09-13 20:07:08 +0530

    rf

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import FooterIcon from './FooterIcon'
 
 const Footer = () => {
   return (
     <div className=' flex justify-evenly items-center py-4 bg-slate-600  text-white text-center'>
-      <FooterIcon/>
+      
       Footer
     </div>
   )
